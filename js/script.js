97e593e2872d0c9c1fa78b626b0a47c48d059d3c
francescoferilli95 <francesco.ferilli1995@libero.it>
2021-03-20 13:34:28 +0100

    adding price calculator based on the age

--- js/script.js
+++ js/script.js
@@ -9,3 +9,32 @@ console.log('Hello Console!');
  *  4. GIVE AN OUTPUT ABOUT THE PRICE WITH ONLY TWO DECIMAL PLACES.
  * 
 */
+
+/**
+ *  VARIABLES
+ */
+
+var kmNeeded = parseInt( prompt('How many KM do you need to do?'));
+console.log(kmNeeded);
+var visitorAge = parseInt( prompt('How old are you?'));
+console.log(visitorAge);
+var price = (0.21 * kmNeeded).toFixed(2);
+console.log(price);
+var youngDiscount = ( (price * 20) / 100).toFixed(2);
+console.log(youngDiscount);
+var seniorDiscount = ( (price * 40) / 100).toFixed(2);
+console.log(seniorDiscount);
+var youngDiscountPrice = (price - youngDiscount).toFixed(2);
+var seniorDiscountPrice = (price - seniorDiscount).toFixed(2);
+
+/**
+ * PRICE CALCULATOR BASED ON THE AGE OF THE VISITOR
+ */
+
+if (visitorAge < 18) {
+    document.getElementById('price').innerHTML = youngDiscountPrice;
+} else if (visitorAge > 65) {
+    document.getElementById('price').innerHTML = seniorDiscountPrice;
+} else {
+    document.getElementById('price').innerHTML = price;
+}
